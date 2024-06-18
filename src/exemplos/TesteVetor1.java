3669294dc74b4b7d73ce9d023a230ff0edc02100
marcelo-etb <mns.etb@gmail.com>
2024-06-17 21:51:11 -0300

    Exemplo de vetor 1

---
+++ src/exemplos/TesteVetor1.java
@@ -0,0 +1,14 @@
+package exemplos;
+
+public class TesteVetor1 {
+    public static void main(String[] args) {
+        int numeros[] = { 10, 20, 30 };
+        double notas[] = new double[10];
+        notas[5] = 7.5;
+        notas[6] = 10;
+        
+        System.out.println(numeros[1]);
+        System.out.println(notas[5]);
+        System.out.println(notas[6]);
+    }
+}
